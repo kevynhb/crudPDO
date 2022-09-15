f9538a51bd547171b4097df41dc53ca9f7f91515
kevyn.hotts <kevyn.hotts@unemat.br>
2022-09-15 16:51:40 -0400

    theory ok

--- crudPDO.php
+++ crudPDO.php
@@ -15,24 +15,25 @@ catch (Exception $e) {
 
 }
 
+
 //----------------------------INSERT------------------------------------
 
 // Duas formas de inserir dados prepare(), query()
-// 1 forma e mais utilizada, serve para passar um parametro e depois substituir
+// 1° forma e mais utilizada, serve para passar um parametro e depois substituir
 // $res = $pdo->prepare("INSERT INTO pessoa(nome, telefone, email)
 //     VALUES (:n, :t, :e)");
 
-// Metodo de subistituir os parametros
-// $res->bindValue(":n","Keyvn"); // aceita váriaveis e funções 
-// $res->bindValue(":t","6854-0000");
-// $res->bindValue(":e","coisa@gmail.com");
-
+// // Metodo de subistituir os parametros
+// $res->bindValue(":n","Joao"); // aceita váriaveis e funções 
+// $res->bindValue(":t","3412-0320");
+// $res->bindValue(":e","joao-da-silva@gmail.com");
 // $res->execute();
 
-// 2 forma de iserir dados no banco, query() não precisa fazer substituição, vai diretamente
+// 2° forma de iserir dados no banco, query() não precisa fazer substituição, vai diretamente
 // passa valores diretamente e quando não se tem parametros para passar.
 // $pdo->query("INSERT INTO pessoa(nome, telefone, email) VALUES 
 //     ('Kevyn', '0932-3230', 'meeEmail@gmail.com')");
+
 
 //----------------------------DELETE------------------------------------
 // $cod = $pdo->prepare("DELETE FROM pessoa WHERE id = :id");
@@ -43,6 +44,7 @@ catch (Exception $e) {
 // $cod = $pdo->query("DELETE FROM pessoa WHERE id = '1'")
 // $cod = $pdo->query("DELETE FROM pessoa") //apaga toda a tabela pessoa
 
+
 //----------------------------UPDATE------------------------------------
 // $cod = $pdo->prepare("UPDATE pessoa SET email = :e WHERE id = :id");
 
@@ -52,4 +54,20 @@ catch (Exception $e) {
 
 // $cod = $pdo->query("UPDATE pessoa SET telefone = '0000-0000' WHERE id = '2'");
 
+
+//----------------------------INSERT------------------------------------
+
+$cod = $pdo->prepare("SELECT * FROM pessoa WHERE id = :id");
+$cod->bindValue(":id", 3);
+$cod->execute();
+// esse comado vai seleciona a informação pedida e vai guardar na variavel $cod
+// não vem em formato array
+// função para transformar essa informação em array
+$resultado = $cod->fetch(PDO::FETCH_ASSOC); // parametro PDO para retornar apenas os nomes das colunas do BD
+ //// fetch: uma unica linha/registro do banco de dados
+// $cod->fetchAll(); // mais de um registro do BD
+
+foreach ($resultado as $key => $value){
+    echo $key.": ".$value."<br>";
+}
 ?>
